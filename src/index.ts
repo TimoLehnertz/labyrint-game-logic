272c9b28a1770529c08cf1371a62e8abadec0c8e
TimoLehnertz <57093983+TimoLehnertz@users.noreply.github.com>
2024-03-19 13:12:49 +0100

    version 1.0.5

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+export { AllPlayerStates } from "./AllPlayerStates";
+export { Board } from "./Board";
+export { BoardPosition } from "./BoardPosition";
+export { PlayerListener, GameSetup, WinListener, Game } from "./Game";
+export { GameState } from "./GameState";
+export { Heading } from "./Heading";
+export { Move } from "./Move";
+export { generateMoves, generateShiftPositions } from "./MoveGenerator";
+export { OpenSides } from "./OpenSides";
+export { Path } from "./Path";
+export { PathTile, TileType } from "./PathTile";
+export { PlayerState } from "./PlayerState";
+export { RandomNumberGenerator } from "./RandomNumberGenerator";
+export { ShiftPosition } from "./ShiftPosition";
+export { Treasure } from "./Treasure";
+export { Vec2 } from "./Vec2";
